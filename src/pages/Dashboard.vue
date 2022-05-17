9f889346913d3a79b8c473b5aa73f4fe338957c0
ibnuprtma <pibnu86@gmail.com>
2022-05-17 11:30:51 +0700

    fix env app name

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,8 +3,8 @@
 		<div class="col-12 lg:col-12 xl:col-12">
 			<div class="surface-section px-4 py-8 md:px-6 lg:px-8">
 				<div class="text-700 text-center">
-					<div class="text-blue-600 font-bold mb-3">&nbsp;SELAMAT DATANG</div>
-					<div class="text-900 font-bold text-5xl mb-3">Semoga Harimu Menyenangkan</div>
+					<div class="text-blue-600 font-bold mb-3">&nbsp;SELAMAT DATANG DI</div>
+					<div class="text-900 font-bold text-5xl mb-3">Web Report - {{appsName}}</div>
 				</div>
 			</div>
 		</div>
@@ -25,6 +25,11 @@ import ExampleChart from '../components/dashboard/ExampleChart.vue';
 export default {
 	components: {
 		'ExampleChart': ExampleChart,
-	}
+	},
+	data() {
+		return {		
+			appsName: process.env.VUE_APP_APPS_NAME,
+		}
+	},
 }
 </script>
