3e1c7121bef8f4420f6c7ffd5cec373f34816d1f
ibnuprtma <pibnu86@gmail.com>
2022-05-18 10:00:57 +0700

    add error in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -3,17 +3,27 @@ import Vuex from "vuex";
 
 export default new Vuex.Store({
   state: {
-    errors: []
+    errors: [],
+    errorAdd: [],
+    errorEdit: [],
   },
 
   getters: {
-    errors: state => state.errors
+    errors: state => state.errors,
+    errorAdd: state => state.errorAdd,
+    errorEdit: state => state.errorEdit,
   },
 
   mutations: {
     setErrors(state, errors) {
       state.errors = errors;
-    }
+    },
+    setErrorAdd(state, errorAdd) {
+      state.errorAdd = errorAdd;
+    },
+    setErrorEdit(state, errorEdit) {
+      state.errorEdit = errorEdit;
+    },
   },
 
   actions: {},
