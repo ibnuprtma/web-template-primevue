9215ccade70004f1dcebf5492fc7d05dfdada4f8
ibnuprtma <pibnu86@gmail.com>
2022-05-18 10:33:01 +0700

    remove error add

--- src/store/index.js
+++ src/store/index.js
@@ -4,22 +4,17 @@ import Vuex from "vuex";
 export default new Vuex.Store({
   state: {
     errors: [],
-    errorAdd: [],
     errorEdit: [],
   },
 
   getters: {
     errors: state => state.errors,
-    errorAdd: state => state.errorAdd,
     errorEdit: state => state.errorEdit,
   },
 
   mutations: {
     setErrors(state, errors) {
       state.errors = errors;
-    },
-    setErrorAdd(state, errorAdd) {
-      state.errorAdd = errorAdd;
     },
     setErrorEdit(state, errorEdit) {
       state.errorEdit = errorEdit;
